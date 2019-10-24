a3720f8b5039878acf1072c8c11d521a9f34ed0e
DomBreno <walterobrienqi197@gmail.com>
2019-10-24 01:06:27 -0300

    02(callback => promise)

--- 01-npm/index.js
+++ 01-npm/index.js
@@ -1,1 +1,1 @@
-console.log('Hello from Node.js!')
+console.log('Hello from Node.js!');
--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -1,55 +1,78 @@
-function getUser(callback) {
-  setTimeout(() =>
-    callback(null, {
-      id: 1,
-      name: 'Deivid Breno',
-      dateOfBirthday: new Date(),
-    }),
-  )
+function getUser() {
+	return new Promise((resolve, reject) =>
+		setTimeout(() =>
+			resolve({
+				id: 1,
+				name: 'Deivid Breno',
+				dateOfBirthday: new Date(),
+			}),
+		),
+	);
 }
 
-function getTel(userId, callback) {
-  setTimeout(() =>
-    callback(null, {
-      tel: '988234675',
-      ddd: 71,
-    }),
-  )
+function getTel(userId) {
+	return new Promise((resolve, reject) =>
+		setTimeout(() =>
+			resolve(
+				{
+					tel: '988234675',
+					ddd: 71,
+				},
+				2000,
+			),
+		),
+	);
 }
 
-function getAddress(userId, callback) {
-  setTimeout(
-    () =>
-      callback(null, {
-        street: 'Abbey Road',
-        number: 3,
-      }),
-    2000,
-  )
+function getAddress(userId) {
+	return new Promise((resolve, reject) =>
+		setTimeout(
+			() =>
+				resolve({
+					street: 'Abbey Road',
+					number: 3,
+				}),
+			2000,
+		),
+	);
 }
 
-getUser((userError, user) => {
-  if (userError) {
-    console.error(`User failure: ${userError}`)
-    return
-  }
+const userPromise = getUser();
+const telPromise = getTel();
 
-  getTel(user.id, (telError, tel) => {
-    if (telError) {
-      console.log(`Tel failure: ${telError}`)
-      return
-    }
-    getAddress(user.id, (addressError, address) => {
-      if (addressError) {
-        console.log(`Address failure: ${addressError}`)
-        return
-      }
+userPromise
+	.then(user =>
+		telPromise.then(tel =>
+			console.log(`
+          User name:  ${user.name}
+          User tel:   (${tel.ddd}) ${tel.tel}
+      `),
+		),
+	)
+	.catch(error => console.error('Something failed:\n', error));
 
-      console.log(`
-        Name:     ${user.name}
-        Address:  ${address.street}, ${address.number}
-        Tel:      (${tel.ddd}) ${tel.tel}
-        `)
-    })
-  })
-})
+// getUser((userError, user) => {
+//   if (userError) {
+//     console.error(`User failure: ${userError}`)
+//     return
+//   }
+
+//   getTel(user.id, (telError, tel) => {
+//     if (telError) {
+//       console.log(`Tel failure: ${telError}`)
+//       return
+//     }
+//     getAddress(user.id, (addressError, address) => {
+//       if (addressError) {
+//         console.log(`Address failure: ${addressError}`)
+//         return
+//       }
+
+//       console.log(`
+//         Name:     ${user.name}
+//         Address:  ${address.street}, ${address.number}
+//         Tel:      (${tel.ddd}) ${tel.tel}
+//         `)
+//     })
+//   })
+// })
